3ce37b9475333ececd65091d17573198ca5ef846
serhiisaveliev <saveliev.sergeyne@gmail.com>
2015-08-23 22:39:59 +0300

    Create movies.js

---
+++ Modules/public/movies.js
@@ -0,0 +1,25 @@
+module.exports = {
+
+  printAvatar: function() {
+    console.log('Avatar');
+  },
+
+  printChappie: function() {
+    console.log('Chappie');
+  },
+
+  favMovie: 'The Matrix'
+
+};
+
+
+//function printAvatar() {
+//  console.log('Avatar: PG-13');
+//}
+//
+//function printChappie() {
+//  console.log('Chappie: R');
+//}
+//
+//module.exports.avatar = printAvatar;
+
