a8793f0f074e255f2acda5843752b6b4b77b1ba3
serhiisaveliev <saveliev.sergeyne@gmail.com>
2015-08-24 13:09:16 +0300

    Create serhii.js

---
+++ 11---Object-Factory/public/serhii.js
@@ -0,0 +1,11 @@
+var movies = require('./movies');
+
+var serhii = movies();
+
+serhii.favMovie = 'Srar Wars';
+
+console.log("Serhii's favorite movie is " + serhii.favMovie);
+
+//var movies = require('./movies');
+//
+//console.log('Serhiis favorite movie is ' + movies.favMovie);
